1ee7282847f2ae179f3be62125b2c01b95baa026
annasri <samysamsam10001@gmail.com>
2025-05-28 13:42:20 +1200

    update data + name folder

---
+++ utils/train_with_validation.py
@@ -0,0 +1,18 @@
+import torch
+
+def train_model(model, loss_fn, optimizer, num_epochs, train_loader):
+    for epoch in range(num_epochs):
+        model.train()
+        running_loss = 0.0
+
+        for inputs, labels in train_loader:
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = loss_fn(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+
+        avg_train_loss = running_loss / len(train_loader)
+        print(f"Epoch [{epoch+1}/{num_epochs}], Train Loss: {avg_train_loss:.4f}")
+
