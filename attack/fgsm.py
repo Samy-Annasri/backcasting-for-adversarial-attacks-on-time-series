ea04f5b1f1165e3f74490d2e0952628949ced913
Annasri Samy <samyannasri.pro@gmail.com>
2025-06-06 14:21:56 +1200

    Delete attack/fgsm.py

--- attack/fgsm.py
+++
@@ -1,9 +0,0 @@
-def fgsm_attack(model, x, y_true, loss_fn, epsilon):
-    x_adv = x.clone().detach().requires_grad_(True)
-    y_pred = model(x_adv)
-    loss = loss_fn(y_pred, y_true)
-    loss.backward()
-    perturbation = epsilon * x_adv.grad.sign()
-    x_adv = x_adv + perturbation
-    return x_adv.detach()
-
