15021f33253a7aa6db42f68ae870fe4b3d10ab1f
Annasri Samy <samyannasri.pro@gmail.com>
2025-07-21 15:34:19 +1200

    Update fgsm.py

--- attack/fgsm.py
+++ attack/fgsm.py
@@ -3,13 +3,15 @@ import numpy as np
 
 def fgsm_attack(model, data_loader, loss_fn, epsilon, price_min, price_max):
     model.eval()
+    device = next(model.parameters()).device
     adv_predictions = []
     true_values = []
 
     for x, y in data_loader:
-        x = x.clone().detach().requires_grad_(True)
-        y = y.clone().detach()
+        x = x.to(device).clone().detach().requires_grad_(True)
+        y = y.to(device).clone().detach()
 
+        model.train()
         # Forward pass
         output = model(x)
         loss = loss_fn(output, y)
@@ -17,6 +19,8 @@ def fgsm_attack(model, data_loader, loss_fn, epsilon, price_min, price_max):
         # Backward pass
         model.zero_grad()
         loss.backward()
+
+        model.eval()
 
         # FGSM perturbation
         data_grad = x.grad.data
@@ -28,8 +32,8 @@ def fgsm_attack(model, data_loader, loss_fn, epsilon, price_min, price_max):
         with torch.no_grad():
             adv_output = model(perturbed_x)
 
-        adv_predictions.extend(adv_output.squeeze().numpy())
-        true_values.extend(y.squeeze().numpy())
+        adv_predictions.extend(adv_output.squeeze().detach().cpu().numpy())
+        true_values.extend(y.squeeze().detach().cpu().numpy())
 
     # Denormalize
     adv_predictions = np.array(adv_predictions) * (price_max - price_min) + price_min
