d8537b76525e20fa1e04ddd3f2c760bc6f678389
Annasri Samy <samyannasri.pro@gmail.com>
2025-07-21 13:29:58 +1200

    Update setup_elec.py

--- utils/elec/setup_elec.py
+++ utils/elec/setup_elec.py
@@ -1,24 +1,35 @@
 import numpy as np
 import torch
-from torch.utils.data import DataLoader, Dataset,TensorDataset
-
+from torch.utils.data import DataLoader, TensorDataset
 
 def prepare_electric_dataset(
     serie,
     sequence_length=30,
     batch_size=64,
-    split_ratio=0.8
+    split_ratio=0.8,
+    reverse=False
 ):
+    serie = np.array(serie)
+
     serie_min = np.min(serie)
     serie_max = np.max(serie)
     serie_norm = (serie - serie_min) / (serie_max - serie_min)
 
     X, Y, used_dates = [], [], []
 
-    for i in range(len(serie_norm) - sequence_length):
-        X.append(serie_norm[i:i + sequence_length])
-        Y.append(serie_norm[i + sequence_length])
-        used_dates.append(i + sequence_length)
+    if not reverse:
+        for i in range(len(serie_norm) - sequence_length):
+            X.append(serie_norm[i:i + sequence_length])
+            Y.append(serie_norm[i + sequence_length])
+            used_dates.append(i + sequence_length)
+    else:
+        for i in range(sequence_length, len(serie_norm)):
+            seq = serie_norm[i-sequence_length:i][::-1]
+            target_idx = i - sequence_length - 1
+            if target_idx >= 0:
+                X.append(seq)
+                Y.append(serie_norm[target_idx])
+                used_dates.append(target_idx)
 
     X = np.array(X)
     Y = np.array(Y)
@@ -42,6 +53,7 @@ def prepare_electric_dataset(
         'train_loader': train_loader,
         'test_loader': test_loader,
         'train_size': len(X_train),
+        'test_size': len(X_test),
         'min_max': (serie_min, serie_max),
         'dates': dates_train + dates_test,
         'X': torch.tensor(X, dtype=torch.float32).unsqueeze(-1),
